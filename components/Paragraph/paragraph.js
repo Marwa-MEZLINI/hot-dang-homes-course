147d45493d33a499a188ac617b31c8bdff55e99f
Marwa-MEZLINI <m.mezlini92@gmail.com>
2023-06-08 15:49:53 +0100

    mapping anchor tags internal links

--- components/Paragraph/paragraph.js
+++ components/Paragraph/paragraph.js
@@ -1,10 +1,11 @@
 import { getTextAlign } from "utils/fonts";
+import { relativeToAbsoluteUrls } from "utils/relativeToAbsoluteUrls";
 
 export const Paragraph = ({ textAlign = "left", content, textColor }) => {
     
     return (
         <p 
-            dangerouslySetInnerHTML={{__html: content}} 
+            dangerouslySetInnerHTML={{__html: relativeToAbsoluteUrls(content)}} 
             className={`font-body max-w-5xl mx-auto ${getTextAlign(textAlign)}`}
             style={{color: textColor}}
         />
---
+++ utils/relativeToAbsoluteUrls.js
@@ -0,0 +1,5 @@
+export const relativeToAbsoluteUrls = (htmlString = "") => {
+    return (
+        htmlString.split(process.env.NEXT_PUBLIC_WP_URL).join("")
+    );
+};
